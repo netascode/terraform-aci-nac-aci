ffb61fa1f313d4e34c753bbebd7dc89e6a10be5f
Rasmus Ekman (raekman) <raekman@cisco.com>
2022-10-07 10:22:01 +0200

    Add dns profile validation (#20)

---
+++ validation/rules/302_dns_policy_profile.py
@@ -0,0 +1,18 @@
+class Rule:
+    id = "302"
+    description = "Verify the DNS Policy Profiles"
+    severity = "HIGH"
+
+    @classmethod
+    def match(cls, inventory):
+        results = []
+        try:
+            dns_policy = inventory["apic"]["fabric_policies"]["dns_policies"]
+
+            for x in dns_policy:
+                if len(x.setdefault("providers", [])) > 2:
+                    results.append("apic.fabric_policies.dns_policies.providers - "
+                                   + x.get("name") + " has more than 2 DNS providers")
+        except KeyError:
+            pass
+        return results
