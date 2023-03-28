50d70a08b4bc17ee291fd8e718eb199c03c90a63
danischm <danischm@cisco.com>
2023-03-28 12:07:24 +0200

    Add inb epg encap to inb node address module

--- aci_node_policies.tf
+++ aci_node_policies.tf
@@ -50,16 +50,17 @@ module "aci_node_registration" {
 
 module "aci_inband_node_address" {
   source  = "netascode/inband-node-address/aci"
-  version = "0.1.3"
+  version = "0.2.0"
 
-  for_each       = { for node in try(local.node_policies.nodes, []) : node.id => node if(try(node.inb_address, null) != null || try(node.inb_v6_address, null) != null) && try(local.modules.aci_inband_node_address, true) && var.manage_node_policies }
-  node_id        = each.value.id
-  pod_id         = try(each.value.pod, local.defaults.apic.node_policies.nodes.pod)
-  ip             = try(each.value.inb_address, "")
-  gateway        = try(each.value.inb_gateway, "")
-  v6_ip          = try(each.value.inb_v6_address, "")
-  v6_gateway     = try(each.value.inb_v6_gateway, "")
-  endpoint_group = try(local.node_policies.inb_endpoint_group, local.defaults.apic.node_policies.inb_endpoint_group)
+  for_each            = { for node in try(local.node_policies.nodes, []) : node.id => node if(try(node.inb_address, null) != null || try(node.inb_v6_address, null) != null) && try(local.modules.aci_inband_node_address, true) && var.manage_node_policies }
+  node_id             = each.value.id
+  pod_id              = try(each.value.pod, local.defaults.apic.node_policies.nodes.pod)
+  ip                  = try(each.value.inb_address, "")
+  gateway             = try(each.value.inb_gateway, "")
+  v6_ip               = try(each.value.inb_v6_address, "")
+  v6_gateway          = try(each.value.inb_v6_gateway, "")
+  endpoint_group      = try(local.node_policies.inb_endpoint_group, local.defaults.apic.node_policies.inb_endpoint_group)
+  endpoint_group_vlan = [for epg in local.inband_endpoint_groups : epg.vlan if epg.name == try(local.node_policies.inb_endpoint_group, local.defaults.apic.node_policies.inb_endpoint_group)][0]
 }
 
 module "aci_oob_node_address" {
