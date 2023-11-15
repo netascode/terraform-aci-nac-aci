7d2c99132096dd098e984d94e359b86fb9fac3a1
Andrew Byrne <66340892+andbyrne@users.noreply.github.com>
2023-11-16 02:22:48 +1100

    Add support for direct connect in service graph templates (#30)

--- modules/terraform-aci-service-graph-template/examples/complete/main.tf
+++ modules/terraform-aci-service-graph-template/examples/complete/main.tf
@@ -2,16 +2,18 @@ module "aci_service_graph_template" {
   source  = "netascode/nac-aci/aci//modules/terraform-aci-service-graph-template"
   version = ">= 0.8.0"
 
-  tenant              = "ABC"
-  name                = "SGT1"
-  alias               = "SGT1-ALIAS"
-  description         = "My Description"
-  template_type       = "FW_ROUTED"
-  redirect            = true
-  share_encapsulation = true
-  device_name         = "DEV1"
-  device_tenant       = "DEF"
-  device_function     = "GoThrough"
-  device_copy         = false
-  device_managed      = false
+  tenant                  = "ABC"
+  name                    = "SGT1"
+  alias                   = "SGT1-ALIAS"
+  description             = "My Description"
+  template_type           = "FW_ROUTED"
+  redirect                = true
+  share_encapsulation     = true
+  device_name             = "DEV1"
+  device_tenant           = "DEF"
+  device_function         = "GoThrough"
+  device_copy             = false
+  device_managed          = false
+  consumer_direct_connect = false
+  provider_direct_connect = true
 }
