496cf57a4e116c58ce15aae2b26ce54227f9f35d
Guilin Yan <guyan@cisco.com>
2023-11-24 02:43:40 -0500

    Add a validation rule on the name variable to avoid Fabric L2 MTU/Port MTU size overlapping configuration

--- modules/terraform-aci-l2-mtu-policy/variables.tf
+++ modules/terraform-aci-l2-mtu-policy/variables.tf
@@ -3,8 +3,8 @@ variable "name" {
   type        = string
 
   validation {
-    condition     = can(regex("^[a-zA-Z0-9_.-]{0,64}$", var.name))
-    error_message = "Allowed characters: `a`-`z`, `A`-`Z`, `0`-`9`, `_`, `.`, `-`. Maximum characters: 64."
+    condition     = can(regex("^[a-zA-Z0-9_.-]{0,64}$", var.name)) && var.name != "default"
+    error_message = "Name `default` is not allowed. Allowed characters: `a`-`z`, `A`-`Z`, `0`-`9`, `_`, `.`, `-`. Maximum characters: 64."
   }
 }
 
