b51f58bbedfce1c3c72d09c7d5a9518228308fb0
bjorndewulf <bjorn.dewulf@gmail.com>
2024-01-26 11:27:30 +0100

    TSHOOT - Trying something - 1

--- modules/terraform-aci-link-level-policy/outputs.tf
+++ modules/terraform-aci-link-level-policy/outputs.tf
@@ -1,5 +1,10 @@
 output "dn" {
-  value       = aci_rest_managed.fabricHIfPol[0].id
+  value       = "${var.physical_media_type == "auto" ? aci_rest_managed.fabricHIfPol[0].id : ""}"
+  description = "Distinguished name of `fabricHIfPol` object."
+}
+
+output "dn2" {
+  value       = "${var.physical_media_type == "sfp-10g-tx" ? aci_rest_managed.fabricHIfPol_sfp-10g-tx[0].id : ""}"
   description = "Distinguished name of `fabricHIfPol` object."
 }
 
