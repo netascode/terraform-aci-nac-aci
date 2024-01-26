913ad259212f7291498cc24c89f26964ba5aeeef
bjorndewulf <bjorn.dewulf@gmail.com>
2024-01-26 10:26:16 +0100

    CORR - Updated Index

--- modules/terraform-aci-link-level-policy/outputs.tf
+++ modules/terraform-aci-link-level-policy/outputs.tf
@@ -1,5 +1,5 @@
 output "dn" {
-  value       = aci_rest_managed.fabricHIfPol.id
+  value       = aci_rest_managed.fabricHIfPol[0].id
   description = "Distinguished name of `fabricHIfPol` object."
 }
 
