a7ff97f7450aa6ed72cec30d0db9c991b61b782e
bdewulfPersonal <bjorn.dewulf@gmail.com>
2024-01-29 17:54:57 +0100

    UPDATE - Ran terraform fmt

--- modules/terraform-aci-link-level-policy/main.tf
+++ modules/terraform-aci-link-level-policy/main.tf
@@ -2,11 +2,11 @@ resource "aci_rest_managed" "fabricHIfPol" {
   dn         = "uni/infra/hintfpol-${var.name}"
   class_name = "fabricHIfPol"
   content = {
-    name    = var.name
-    speed   = var.speed
-    autoNeg = var.auto == true ? "on" : "off"
-    fecMode = var.fec_mode
-    portPhyMediaType = try(var.physical_media_type, null) != null ? var.physical_media_type : null 
+    name             = var.name
+    speed            = var.speed
+    autoNeg          = var.auto == true ? "on" : "off"
+    fecMode          = var.fec_mode
+    portPhyMediaType = try(var.physical_media_type, null) != null ? var.physical_media_type : null
   }
 }
 
