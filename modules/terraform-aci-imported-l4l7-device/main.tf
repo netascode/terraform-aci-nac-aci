7b802f54a003a3ba6a0a920185ace702a9c9b7ca
danischm <danischm@cisco.com>
2024-01-30 20:50:26 +0100

    Fix typo

--- modules/terraform-aci-imported-l4l7-device/main.tf
+++ modules/terraform-aci-imported-l4l7-device/main.tf
@@ -2,7 +2,7 @@ resource "aci_rest_managed" "vnsLDevIf" {
   dn         = "uni/tn-${var.tenant}/lDevIf-[uni/tn-${var.source_tenant}/lDevVip-${var.source_device}]"
   class_name = "vnsLDevIf"
   content = {
-    ldev        = "uni/tn-${var.source_tenant}/lDevVip-${var.source_device}]"
+    ldev        = "uni/tn-${var.source_tenant}/lDevVip-${var.source_device}"
     description = var.description
   }
 }
