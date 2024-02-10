1ee96cc74c17585e98c3dfb6b898ecf02c0fc2e3
danischm <danischm@cisco.com>
2024-02-10 16:01:07 +0100

    Formatting fixes

--- validation/rules/305_aaa_password_class_flags.py
+++ validation/rules/305_aaa_password_class_flags.py
@@ -8,18 +8,21 @@ class Rule:
         results = []
         try:
 
-            password_class_flags = inventory["apic"]["fabric_policies"]["aaa"]['management_settings']['password_strength_profile']['password_class_flags']
-            
+            password_class_flags = inventory["apic"]["fabric_policies"]["aaa"][
+                "management_settings"
+            ]["password_strength_profile"]["password_class_flags"]
+
             # insert the list to the set
             list_set = set(password_class_flags)
             # convert the set to the list
-            unique_list = (list(list_set))
+            unique_list = list(list_set)
 
             if len(unique_list) < 3 or len(unique_list) > 4:
                 results.append(
-                    "apic.fabric_policies.aaa.management_settings.password_class_flags - " +
-                     str(password_class_flags) +
-                     " is not a combination of at least three out of four options: digits,lowercase,specialchars,uppercase.")
+                    "apic.fabric_policies.aaa.management_settings.password_class_flags - "
+                    + str(password_class_flags)
+                    + " is not a combination of at least three out of four options: digits,lowercase,specialchars,uppercase."
+                )
         except KeyError:
             pass
         return results
