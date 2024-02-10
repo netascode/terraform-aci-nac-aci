3cbea3248e6119ffa26f48291dff02c1e9a05eb8
danischm <danischm@cisco.com>
2024-02-10 15:40:09 +0100

    Formatting fixes

--- validation/rules/304_tenant_redirect_policy_type.py
+++ validation/rules/304_tenant_redirect_policy_type.py
@@ -10,28 +10,30 @@ class Rule:
             has_redirect_policy = False
             tenants = inventory.get("apic", {}).get("tenants", {})
             for tenant in tenants:
-                for redirect_policies in tenant.get("services", {}).get("redirect_policies", {}):
+                for redirect_policies in tenant.get("services", {}).get(
+                    "redirect_policies", {}
+                ):
                     if redirect_policies.get("type", ""):
                         has_redirect_policy = True
 
             if has_redirect_policy:
                 for tenant in tenants:
-                    for redirect_policies in tenant.get("services", {}).get("redirect_policies", {}):
-                        if (
-                                redirect_policies.get("type", "L3") == "L3"
-                                and
-                                redirect_policies.get("l1l2_destinations", {})
-                        ):
-                            results.append("l1l2_destinations should have DestType as L1/L2")
+                    for redirect_policies in tenant.get("services", {}).get(
+                        "redirect_policies", {}
+                    ):
+                        if redirect_policies.get(
+                            "type", "L3"
+                        ) == "L3" and redirect_policies.get("l1l2_destinations", {}):
+                            results.append(
+                                "l1l2_destinations should have DestType as L1/L2"
+                            )
 
-                        if (
-                                redirect_policies.get("type", "L3") in ["L1", "L2"]
-                                and
-                                redirect_policies.get("l3_destinations", {})
-                        ):
+                        if redirect_policies.get("type", "L3") in [
+                            "L1",
+                            "L2",
+                        ] and redirect_policies.get("l3_destinations", {}):
                             results.append("l3_destinations should have DestType as L3")
-                
-                
+
         except KeyError:
             pass
         return results
