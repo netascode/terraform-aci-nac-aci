1b7cec69079a700304dd5471446af2527afe1869
Maxiturne <38961513+Maxiturne@users.noreply.github.com>
2024-02-19 13:29:00 +0100

    Update variables.tf
    Adding support for ":" in contracts by changing the regex validation in the variables.tf files

--- modules/terraform-aci-contract/variables.tf
+++ modules/terraform-aci-contract/variables.tf
@@ -14,7 +14,7 @@ variable "name" {
 
   validation {
     condition     = can(regex("^[a-zA-Z0-9_.:-]{0,64}$", var.name))
-    error_message = "Allowed characters: `a`-`z`, `A`-`Z`, `0`-`9`, `_`, `.`, `-`. Maximum characters: 64."
+    error_message = "Allowed characters: `a`-`z`, `A`-`Z`, `0`-`9`, `_`, `.`, `:`, `-`. Maximum characters: 64."
   }
 }
 
