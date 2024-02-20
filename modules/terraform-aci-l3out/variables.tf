fa799e26090ce4ff75f2d99b13328a1b95124cac
lab <juchowan@cisco.com>
2024-02-20 15:18:45 +0100

    vars cleanup

--- modules/terraform-aci-l3out/variables.tf
+++ modules/terraform-aci-l3out/variables.tf
@@ -466,25 +466,3 @@ variable "sr_mpls_infra_l3outs" {
     error_message = "`external_epgs`: Allowed characters: `a`-`z`, `A`-`Z`, `0`-`9`, `_`, `.`, `-`. Maximum characters: 64."
   }
 }
-
-variable "sr_mpls_inbound_route_map" {
-  description = "SR MPLS Tenant L3out Inbound Route Map name."
-  type        = string
-  default     = ""
-
-  validation {
-    condition     = can(regex("^[a-zA-Z0-9_.-]{0,64}$", var.sr_mpls_inbound_route_map))
-    error_message = "Allowed characters: `a`-`z`, `A`-`Z`, `0`-`9`, `_`, `.`, `-`. Maximum characters: 64."
-  }
-}
-
-variable "sr_mpls_outbound_route_map" {
-  description = "SR MPLS Tenant L3out Outbound Route Map name."
-  type        = string
-  default     = ""
-
-  validation {
-    condition     = can(regex("^[a-zA-Z0-9_.-]{0,64}$", var.sr_mpls_outbound_route_map))
-    error_message = "Allowed characters: `a`-`z`, `A`-`Z`, `0`-`9`, `_`, `.`, `-`. Maximum characters: 64."
-  }
-}
