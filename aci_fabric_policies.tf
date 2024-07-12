c7914a824144f19b9e6ecec5166b38aa99deaf11
Andrew Byrne <andbyrne@cisco.com>
2024-07-12 16:57:31 +1000

    Fix backwards compatibility

--- aci_fabric_policies.tf
+++ aci_fabric_policies.tf
@@ -28,8 +28,8 @@ module "aci_endpoint_loop_protection" {
 
   count                = local.modules.aci_endpoint_loop_protection == true && var.manage_fabric_policies ? 1 : 0
   admin_state          = try(local.fabric_policies.ep_loop_protection.admin_state, local.defaults.apic.fabric_policies.ep_loop_protection.admin_state)
-  bd_learn_disable     = try(local.fabric_policies.ep_loop_protection.bd_learn_disable, local.fabric_policies.ep_loop_protection.action == "bd-learn-disable" ? true : false, local.defaults.apic.fabric_policies.ep_loop_protection.bd_learn_disable)
-  port_disable         = try(local.fabric_policies.ep_loop_protection.port_disable, local.fabric_policies.ep_loop_protection.action == "port-disable" ? true : false, local.defaults.apic.fabric_policies.ep_loop_protection.port_disable)
+  bd_learn_disable     = try(local.fabric_policies.ep_loop_protection.action == "bd-learn-disable" ? true : false, local.defaults.apic.fabric_policies.ep_loop_protection.action == "bd-learn-disable" ? true : false, local.fabric_policies.ep_loop_protection.bd_learn_disable, local.defaults.apic.fabric_policies.ep_loop_protection.bd_learn_disable)
+  port_disable         = try(local.fabric_policies.ep_loop_protection.action == "port-disable" ? true : false, local.defaults.apic.fabric_policies.ep_loop_protection.action == "port-disable" ? true : false, local.fabric_policies.ep_loop_protection.port_disable, local.defaults.apic.fabric_policies.ep_loop_protection.port_disable)
   detection_interval   = try(local.fabric_policies.ep_loop_protection.detection_interval, local.defaults.apic.fabric_policies.ep_loop_protection.detection_interval)
   detection_multiplier = try(local.fabric_policies.ep_loop_protection.detection_multiplier, local.defaults.apic.fabric_policies.ep_loop_protection.detection_multiplier)
 }
