0f02645fea7501595078522020e32e0ba502611b
Jorge Gomez Velasquez <jorgegomez2307@hotmail.com>
2024-08-23 07:37:04 -0500

    Support PVLAN (#129)
    * Support PVLAN
    
    * Add documentation
    
    * docs
    
    ---------
    
    Co-authored-by: jgomezve <jgomezve@cisco.com>
    Co-authored-by: student <juchowan@cisco.com>

--- modules/terraform-aci-endpoint-group/examples/complete/main.tf
+++ modules/terraform-aci-endpoint-group/examples/complete/main.tf
@@ -1,6 +1,6 @@
 module "aci_endpoint_group" {
   source  = "netascode/nac-aci/aci//modules/terraform-aci-endpoint-group"
-  version = ">= 0.8.0"
+  version = ">= 0.9.1"
 
   tenant                      = "ABC"
   application_profile         = "AP1"
@@ -52,6 +52,7 @@ module "aci_endpoint_group" {
   static_ports = [{
     node_id              = 101
     vlan                 = 123
+    primary_vlan         = 124
     pod_id               = 1
     port                 = 10
     sub_port             = 1
