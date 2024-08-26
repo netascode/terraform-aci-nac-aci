5c660026823e81dd5101b570c9ee880d41aa34f4
Jorge Gomez Velasquez <jorgegomez2307@hotmail.com>
2024-08-26 04:27:41 -0500

    Support BPG profiles (#136)
    * Support BPG profiles
    
    * Add documentation
    
    * Add documentation
    
    ---------
    
    Co-authored-by: jgomezve <jgomezve@cisco.com>

--- modules/terraform-aci-l3out-node-profile/variables.tf
+++ modules/terraform-aci-l3out-node-profile/variables.tf
@@ -271,3 +271,26 @@ variable "bfd_multihop_node_policy" {
     error_message = "Allowed characters: `a`-`z`, `A`-`Z`, `0`-`9`, `_`, `.`, `:`, `-`. Maximum characters: 64."
   }
 }
+
+variable "bgp_timer_policy" {
+  description = "Node Profile's BGP Timer Policy"
+  type        = string
+  default     = ""
+
+  validation {
+    condition     = can(regex("^[a-zA-Z0-9_.:-]{0,64}$", var.bgp_timer_policy))
+    error_message = "Allowed characters: `a`-`z`, `A`-`Z`, `0`-`9`, `_`, `.`, `:`, `-`. Maximum characters: 64."
+  }
+}
+
+
+variable "bgp_as_path_policy" {
+  description = "Node Profile's BGP AS-Path Policy"
+  type        = string
+  default     = ""
+
+  validation {
+    condition     = can(regex("^[a-zA-Z0-9_.:-]{0,64}$", var.bgp_as_path_policy))
+    error_message = "Allowed characters: `a`-`z`, `A`-`Z`, `0`-`9`, `_`, `.`, `:`, `-`. Maximum characters: 64."
+  }
+}
