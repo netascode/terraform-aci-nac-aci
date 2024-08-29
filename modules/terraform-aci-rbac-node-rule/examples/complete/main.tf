5e1bf5dc351429547b4807307d9beacb42c8e2f6
Justyna Chowaniec <79261946+juchowan@users.noreply.github.com>
2024-08-29 15:30:22 +0200

    Update main.tf

--- modules/terraform-aci-rbac-node-rule/examples/complete/main.tf
+++ modules/terraform-aci-rbac-node-rule/examples/complete/main.tf
@@ -1,6 +1,6 @@
 module "aci_rbac_node_rule" {
   source  = "netascode/nac-aci/aci//modules/terraform-aci-rbac-node-rule"
-  version = ">= 0.9.0"
+  version = ">= 0.9.1"
 
   node_id = 101
   port_rules = [{
