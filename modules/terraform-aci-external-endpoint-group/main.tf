91c7d62a528bd01e417b1a97fff46d9483bfe1d8
juchowan <juchowan@cisco.com>
2024-10-22 17:07:31 +0200

    docs

--- modules/terraform-aci-external-endpoint-group/main.tf
+++ modules/terraform-aci-external-endpoint-group/main.tf
@@ -86,7 +86,7 @@ resource "aci_rest_managed" "l3extRsSubnetToProfile" {
 }
 
 resource "aci_rest_managed" "l3extRsSubnetToRtSumm" {
-  for_each = { for subnet in var.subnets : subnet.prefix => subnet if subnet.bgp_route_summarization || subnet.ospf_route_summarization || subnet.eigrp_route_summarization }
+  for_each   = { for subnet in var.subnets : subnet.prefix => subnet if subnet.bgp_route_summarization || subnet.ospf_route_summarization || subnet.eigrp_route_summarization }
   dn         = "${aci_rest_managed.l3extSubnet[each.value.prefix].dn}/rsSubnetToRtSumm"
   class_name = "l3extRsSubnetToRtSumm"
   content = {
