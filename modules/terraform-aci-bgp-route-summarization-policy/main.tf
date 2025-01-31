94f2872502b86a4d1328275d8bbcbc4edc688864
ayabbas <ayabbas@cisco.com>
2025-01-31 13:46:46 +0000

    moved addTCtrl to local var to be added to map conditionally

--- modules/terraform-aci-bgp-route-summarization-policy/main.tf
+++ modules/terraform-aci-bgp-route-summarization-policy/main.tf
@@ -1,10 +1,22 @@
+locals {
+  addrTCtrl = length(join(",", concat(var.af_mcast == true ? ["af-mcast"] : [], var.af_ucast == true ? ["af-ucast"] : []))) > 0 ? join(",", concat(var.af_mcast == true ? ["af-mcast"] : [], var.af_ucast == true ? ["af-ucast"] : [])) : null
+}
+variable "content_map" {
+  type    = map(string)
+  default = {}
+}
 resource "aci_rest_managed" "bgpRtSummPol" {
+
   dn         = "uni/tn-${var.tenant}/bgprtsum-${var.name}"
   class_name = "bgpRtSummPol"
-  content = {
-    name      = var.name
-    descr     = var.description
-    ctrl      = join(",", concat(var.as_set == true ? ["as-set"] : [], var.summary_only == true ? ["summary-only"] : []))
-    addrTCtrl = join(",", concat(var.af_mcast == true ? ["af-mcast"] : [], var.af_ucast == true ? ["af-ucast"] : []))
-  }
-}
+  content = merge(
+    var.content_map,
+    {
+      name  = var.name,
+      descr = var.description,
+      ctrl  = join(",", concat(var.as_set == true ? ["as-set"] : [], var.summary_only == true ? ["summary-only"] : []))
+    },
+    local.addrTCtrl != null ? { addrTCtrl = local.addrTCtrl } : {}
+  )
+
+} 
