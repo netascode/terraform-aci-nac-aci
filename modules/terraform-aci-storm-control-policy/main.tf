a1f1dcc7279372163db6568a868394db3686d1ec
Andrew Byrne <66340892+andbyrne@users.noreply.github.com>
2025-03-11 20:44:29 +1100

    Format storm control rates to 6 decimal places (#212)

--- modules/terraform-aci-storm-control-policy/main.tf
+++ modules/terraform-aci-storm-control-policy/main.tf
@@ -9,21 +9,21 @@ resource "aci_rest_managed" "stormctrlIfPol" {
     type                     = "all"
     isUcMcBcStormPktCfgValid = var.configuration_type == "separate" ? "Valid" : "Invalid"
     burstPps                 = var.burst_pps
-    burstRate                = var.burst_rate
-    rate                     = var.rate
+    burstRate                = format("%.6f", var.burst_rate)
+    rate                     = format("%.6f", var.rate)
     ratePps                  = var.rate_pps
     stormCtrlSoakInstCount   = "3"
     bcBurstPps               = var.broadcast_burst_pps
-    bcBurstRate              = var.broadcast_burst_rate
+    bcBurstRate              = format("%.6f", var.broadcast_burst_rate)
     bcRatePps                = var.broadcast_pps
-    bcRate                   = var.broadcast_rate
+    bcRate                   = format("%.6f", var.broadcast_rate)
     mcBurstPps               = var.multicast_burst_pps
-    mcBurstRate              = var.multicast_burst_rate
+    mcBurstRate              = format("%.6f", var.multicast_burst_rate)
     mcRatePps                = var.multicast_pps
-    mcRate                   = var.multicast_rate
+    mcRate                   = format("%.6f", var.multicast_rate)
     uucBurstPps              = var.unknown_unicast_burst_pps
-    uucBurstRate             = var.unknown_unicast_burst_rate
+    uucBurstRate             = format("%.6f", var.unknown_unicast_burst_rate)
     uucRatePps               = var.unknown_unicast_pps
-    uucRate                  = var.unknown_unicast_rate
+    uucRate                  = format("%.6f", var.unknown_unicast_rate)
   }
 }
