e7dda14459ac2d6072ed4179230a9cdd4e675acc
egrosdou <egrosdou@cisco.com>
2025-04-03 14:40:17 +0200

    Update files with TF format

--- modules/terraform-aci-link-level-policy/examples/complete/main.tf
+++ modules/terraform-aci-link-level-policy/examples/complete/main.tf
@@ -2,10 +2,10 @@ module "aci_link_level_policy" {
   source  = "netascode/nac-aci/aci//modules/terraform-aci-link-level-policy"
   version = ">= 0.8.0"
 
-  name     = "100G"
-  speed    = "100G"
-  link_delay_ms = 10
+  name             = "100G"
+  speed            = "100G"
+  link_delay_ms    = 10
   link_debounce_ms = 110
-  auto     = true
-  fec_mode = "disable-fec"
+  auto             = true
+  fec_mode         = "disable-fec"
 }
