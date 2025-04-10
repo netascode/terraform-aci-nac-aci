6e5952cf10295e043e1fe67b462e1a75186ead4e
jmiguelhp <jmiguelhp@gmail.com>
2025-04-10 12:20:21 -0600

    SPAN Filter Groups update - entry name optional.

--- modules/terraform-aci-access-span-filter-group/main.tf
+++ modules/terraform-aci-access-span-filter-group/main.tf
@@ -8,7 +8,7 @@ resource "aci_rest_managed" "spanFilterGrp" {
 }
 
 resource "aci_rest_managed" "spanFilterEntry" {
-  for_each   = { for entry in var.entries : entry.name => entry }
+  for_each   = { for entry in var.entries : (entry.name != null && entry.name != "" ? entry.name : "${entry.source_ip}_${entry.destination_ip}") => entry }
   dn         = "${aci_rest_managed.spanFilterGrp.dn}/proto-${each.value.ip_protocol}-src-[${each.value.source_ip}]-dst-[${each.value.destination_ip}]-srcPortFrom-${each.value.source_from_port}-srcPortTo-${each.value.source_to_port != null ? each.value.source_to_port : each.value.source_from_port}-dstPortFrom-${each.value.destination_from_port}-dstPortTo-${each.value.destination_to_port != null ? each.value.destination_to_port : each.value.destination_from_port}"
   class_name = "spanFilterEntry"
   content = {
