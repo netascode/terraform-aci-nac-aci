e4e085a9f8bed89e150cc5fe0b420b0182b8b4bb
hidubey <hidubey@cisco.com>
2025-05-08 13:34:09 +0530

    Added main.tf

--- modules/terraform-aci-cp-mtu/examples/complete/main.tf
+++ modules/terraform-aci-cp-mtu/examples/complete/main.tf
@@ -1,5 +1,5 @@
-module "aci_fabric_l2_mtu" {
-  source  = "netascode/nac-aci/aci//modules/terraform-aci-fabric-l2-mtu"
+module "aci-cp-mtu" {
+  source  = "netascode/nac-aci/aci//modules/terraform-aci-cp-mtu"
   version = ">= 0.8.0"
 
   l2_port_mtu = 9216
