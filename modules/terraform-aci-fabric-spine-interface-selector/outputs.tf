3d58b97846dcf31e408473f9f6debb5360b14a8c
Oskar Gorczowski <ogorczow@cisco.com>
2025-06-11 10:38:10 +0200

    initial_commit

---
+++ modules/terraform-aci-fabric-spine-interface-selector/outputs.tf
@@ -0,0 +1,9 @@
+output "dn" {
+  value       = aci_rest_managed.fabricLFPortS.id
+  description = "Distinguished name of `fabricLFPortS` object."
+}
+
+output "name" {
+  value       = aci_rest_managed.fabricLFPortS.content.name
+  description = "Spine interface selector name."
+}
