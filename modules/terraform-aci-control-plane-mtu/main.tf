81140630fd109f1d80534498b85da2d5bbfd661d
suhas1git <raj.suhaskb@gmail.com>
2025-07-23 19:38:08 +0530

    changes to required varibales on main.tf

--- modules/terraform-aci-control-plane-mtu/main.tf
+++ modules/terraform-aci-control-plane-mtu/main.tf
@@ -2,7 +2,7 @@ resource "aci_rest_managed" "infraCPMtuPol" {
   dn         = "uni/infra/CPMtu"
   class_name = "infraCPMtuPol"
   content = {
-    cpMTU        = var.control_plane_mtu
-    apicMtuApply = var.apic_mtu_apply
+    CPMtu        = var.control_plane_mtu
+    APICMtuApply = var.apic_mtu_apply
   }
 }
