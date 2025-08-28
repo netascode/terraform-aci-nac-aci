c52c8e1332b9ac231b83063b58ca2b73f8a46461
suhas1git <raj.suhaskb@gmail.com>
2025-08-28 18:14:22 +0530

    addressing version and name change as per review comments

--- aci_fabric_policies.tf
+++ aci_fabric_policies.tf
@@ -1142,6 +1142,6 @@ module "aci_atomic_counter" {
 module "aci_control_plane_mtu" {
   source = "./modules/terraform-aci-control-plane-mtu"
 
-  mtu            = try(local.fabric_policies.control_plane_mtu_policy.mtu, local.defaults.apic.fabric_policies.control_plane_mtu_policy.mtu)
-  apic_mtu_apply = try(local.fabric_policies.control_plane_mtu_policy.apic_mtu_apply, local.defaults.apic.fabric_policies.control_plane_mtu_policy.apic_mtu_apply)
+  mtu            = try(local.fabric_policies.control_plane_mtu.mtu, local.defaults.apic.fabric_policies.control_plane_mtu.mtu)
+  apic_mtu_apply = try(local.fabric_policies.control_plane_mtu.apic_mtu_apply, local.defaults.apic.fabric_policies.control_plane_mtu.apic_mtu_apply)
 }
--- modules/terraform-aci-control-plane-mtu/examples/complete/main.tf
+++ modules/terraform-aci-control-plane-mtu/examples/complete/main.tf
@@ -1,6 +1,6 @@
 module "aci_control_plane_mtu" {
   source  = "netascode/nac-aci/aci//modules/terraform-aci-control-plane-mtu"
-  version = ">= 1.1.0"
+  version = "> 1.1.0"
 
   CPMtu        = 9000
   APICMtuApply = true
