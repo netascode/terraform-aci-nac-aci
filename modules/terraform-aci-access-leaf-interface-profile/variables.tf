194ba553e752b077ec181eca16101b85cbd1d70f
tkraner <110914553+tkraner@users.noreply.github.com>
2025-09-25 15:09:07 +0200

    Added description variable for leaf interface profiles (#233)
    * Added description variable for leaf interface profiles
    
    * minor fixes
    
    ---------
    
    Co-authored-by: Thomas Kraner <tkraner.github@overpasser.4wrd.cc>
    Co-authored-by: Justyna Chowaniec <juchowan@cisco.com>

--- modules/terraform-aci-access-leaf-interface-profile/variables.tf
+++ modules/terraform-aci-access-leaf-interface-profile/variables.tf
@@ -7,3 +7,14 @@ variable "name" {
     error_message = "Allowed characters: `a`-`z`, `A`-`Z`, `0`-`9`, `_`, `.`, `:`, `-`. Maximum characters: 64."
   }
 }
+
+variable "description" {
+  description = "Leaf interface profile description."
+  type        = string
+  default     = ""
+
+  validation {
+    condition     = can(regex("^[a-zA-Z0-9\\!#$%()*,-./:;@ _{|}~?&+]{0,128}$", var.description))
+    error_message = "Allowed characters: `a`-`z`, `A`-`Z`, `0`-`9`, `\\`, `!`, `#`, `$`, `%`, `(`, `)`, `*`, `,`, `-`, `.`, `/`, `:`, `;`, `@`, ` `, `_`, `{`, `|`, }`, `~`, `?`, `&`, `+`. Maximum characters: 128."
+  }
+}
