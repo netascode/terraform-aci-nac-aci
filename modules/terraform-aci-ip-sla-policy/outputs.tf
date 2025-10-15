4cafed8959ad4f6bacc47bb1126441875d258c1d
girbalaj <girishbalaji1227@gmail.com>
2025-10-15 18:42:22 +0530

    remove variables from output.tf

--- modules/terraform-aci-ip-sla-policy/outputs.tf
+++ modules/terraform-aci-ip-sla-policy/outputs.tf
@@ -7,23 +7,3 @@ output "name" {
   value       = aci_rest_managed.fvIPSLAMonitoringPol.content.name
   description = "IP SLA Policy name."
 }
-
-output "sla_type" {
-  value       = aci_rest_managed.fvIPSLAMonitoringPol.content.slaType
-  description = "IP SLA Policy type."
-}
-
-output "http_method" {
-  value       = aci_rest_managed.fvIPSLAMonitoringPol.content.httpMethod
-  description = "IP SLA Policy HTTP method."
-}
-
-output "http_version" {
-  value       = aci_rest_managed.fvIPSLAMonitoringPol.content.httpVersion
-  description = "IP SLA Policy HTTP version."
-}
-
-output "http_uri" {
-  value       = aci_rest_managed.fvIPSLAMonitoringPol.content.httpUri
-  description = "IP SLA Policy HTTP URI."
-}
