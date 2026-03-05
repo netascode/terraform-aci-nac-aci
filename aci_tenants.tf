c5621053aaed17333eb86ebfccef36398640654e
Justyna Chowaniec <juchowan@cisco.com>
2026-03-05 13:28:49 +0100

    fix ip_sla_policy

--- aci_tenants.tf
+++ aci_tenants.tf
@@ -3626,7 +3626,7 @@ locals {
         resilient_hashing      = try(policy.resilient_hashing, local.defaults.apic.tenants.services.redirect_policies.resilient_hashing)
         threshold_down_action  = try(policy.threshold_down_action, local.defaults.apic.tenants.services.redirect_policies.threshold_down_action)
         ip_sla_policy          = try("${policy.ip_sla_policy}${local.defaults.apic.tenants.policies.ip_sla_policies.name_suffix}", "")
-        ip_sla_policy_tenant   = !contains(try(local.tenant_shared_policies[tenant.name].ip_sla_policies, []), policy.ip_sla_policy) && contains(try(local.tenant_shared_policies["common"].ip_sla_policies, []), policy.ip_sla_policy) ? "common" : tenant.name
+        ip_sla_policy_tenant   = !contains(try(local.tenant_shared_policies[tenant.name].ip_sla_policies, []), try(policy.ip_sla_policy, "")) && contains(try(local.tenant_shared_policies["common"].ip_sla_policies, []), try(policy.ip_sla_policy, "")) ? "common" : tenant.name
         redirect_backup_policy = try("${policy.redirect_backup_policy}${local.defaults.apic.tenants.services.redirect_backup_policies.name_suffix}", "")
         rewrite_source_mac     = try(policy.rewrite_source_mac, null)
         l3_destinations = [for dest in try(policy.l3_destinations, []) : {
@@ -4136,7 +4136,7 @@ locals {
           scope_type           = policy.scope_type
           scope                = policy.scope
           ip_sla_policy        = policy.ip_sla_policy
-          ip_sla_policy_tenant = !contains(try(local.tenant_shared_policies[tenant.name].ip_sla_policies, []), policy.ip_sla_policy) && contains(try(local.tenant_shared_policies["common"].ip_sla_policies, []), policy.ip_sla_policy) ? "common" : tenant.name
+          ip_sla_policy_tenant = !contains(try(local.tenant_shared_policies[tenant.name].ip_sla_policies, []), try(policy.ip_sla_policy, "")) && contains(try(local.tenant_shared_policies["common"].ip_sla_policies, []), try(policy.ip_sla_policy, "")) ? "common" : tenant.name
         }
       ],
       [
@@ -4154,7 +4154,7 @@ locals {
                     scope_type           = "l3out"
                     scope                = try("${l3out.name}${local.defaults.apic.tenants.l3outs.name_suffix}", "")
                     ip_sla_policy        = try("${next_hop.ip_sla_policy}${local.defaults.apic.tenants.policies.ip_sla_policies.name_suffix}", null)
-                    ip_sla_policy_tenant = !contains(try(local.tenant_shared_policies[tenant.name].ip_sla_policies, []), next_hop.ip_sla_policy) && contains(try(local.tenant_shared_policies["common"].ip_sla_policies, []), next_hop.ip_sla_policy) ? "common" : tenant.name
+                    ip_sla_policy_tenant = !contains(try(local.tenant_shared_policies[tenant.name].ip_sla_policies, []), try(next_hop.ip_sla_policy, "")) && contains(try(local.tenant_shared_policies["common"].ip_sla_policies, []), try(next_hop.ip_sla_policy, "")) ? "common" : tenant.name
                   } if try(next_hop.ip_sla_policy, null) != null
                 ]
               ]
@@ -4172,7 +4172,7 @@ locals {
                   scope_type           = "l3out"
                   scope                = try("${l3out.name}${local.defaults.apic.tenants.l3outs.name_suffix}", "")
                   ip_sla_policy        = try("${next_hop.ip_sla_policy}${local.defaults.apic.tenants.policies.ip_sla_policies.name_suffix}", null)
-                  ip_sla_policy_tenant = !contains(try(local.tenant_shared_policies[tenant.name].ip_sla_policies, []), next_hop.ip_sla_policy) && contains(try(local.tenant_shared_policies["common"].ip_sla_policies, []), next_hop.ip_sla_policy) ? "common" : tenant.name
+                  ip_sla_policy_tenant = !contains(try(local.tenant_shared_policies[tenant.name].ip_sla_policies, []), try(next_hop.ip_sla_policy, "")) && contains(try(local.tenant_shared_policies["common"].ip_sla_policies, []), try(next_hop.ip_sla_policy, "")) ? "common" : tenant.name
                 } if try(next_hop.ip_sla_policy, null) != null
               ]
             ]
