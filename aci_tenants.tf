359ae3eba85772c7fe28814481c215afb3cea330
suhas1git <raj.suhaskb@gmail.com>
2026-06-16 12:24:52 +0530

    Accepted and removed try(tag.value, )

--- aci_tenants.tf
+++ aci_tenants.tf
@@ -357,7 +357,7 @@ locals {
           bridge_domain               = try("${epg.bridge_domain}${local.defaults.apic.tenants.bridge_domains.name_suffix}", "")
           data_plane_policing_policy  = try("${epg.data_plane_policing_policy}${local.defaults.apic.tenants.policies.data_plane_policing_policies.name_suffix}", "")
           tags                        = try(epg.tags, [])
-          tag_annotations             = [for tag in try(epg.tag_annotations, []) : { key = tag.key, value = try(tag.value, "") }]
+          tag_annotations             = [for tag in try(epg.tag_annotations, []) : { key = tag.key, value = tag.value }]
           trust_control_policy        = try("${epg.trust_control_policy}${local.defaults.apic.tenants.policies.trust_control_policies.name_suffix}", "")
           contract_consumers          = try([for contract in epg.contracts.consumers : "${contract}${local.defaults.apic.tenants.contracts.name_suffix}"], [])
           contract_providers          = try([for contract in epg.contracts.providers : "${contract}${local.defaults.apic.tenants.contracts.name_suffix}"], [])
@@ -599,7 +599,7 @@ locals {
           custom_qos_policy           = try("${useg_epg.custom_qos_policy}${local.defaults.apic.tenants.policies.custom_qos.name_suffix}", "")
           bridge_domain               = try("${useg_epg.bridge_domain}${local.defaults.apic.tenants.bridge_domains.name_suffix}", "")
           tags                        = try(useg_epg.tags, [])
-          tag_annotations             = [for tag in try(useg_epg.tag_annotations, []) : { key = tag.key, value = try(tag.value, "") }]
+          tag_annotations             = [for tag in try(useg_epg.tag_annotations, []) : { key = tag.key, value = tag.value }]
           trust_control_policy        = try("${useg_epg.trust_control_policy}${local.defaults.apic.tenants.policies.trust_control_policies.name_suffix}", "")
           contract_consumers          = try([for contract in useg_epg.contracts.consumers : "${contract}${local.defaults.apic.tenants.contracts.name_suffix}"], [])
           contract_providers          = try([for contract in useg_epg.contracts.providers : "${contract}${local.defaults.apic.tenants.contracts.name_suffix}"], [])
