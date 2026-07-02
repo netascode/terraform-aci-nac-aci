ad0c76e5c01cbc26d52b3f377826b4ad63726a80
Justyna Chowaniec <79261946+juchowan@users.noreply.github.com>
2026-07-02 10:07:41 +0200

    add support for default ptp profile (#403)

--- modules/terraform-aci-ptp-profile/main.tf
+++ modules/terraform-aci-ptp-profile/main.tf
@@ -6,7 +6,7 @@ resource "aci_rest_managed" "ptpProfile" {
     announceIntvl        = var.announce_interval
     announceTimeout      = var.announce_timeout
     delayIntvl           = var.delay_interval
-    profileTemplate      = var.template == "telecom" ? "telecom_full_path" : var.template == "smpte" ? "smpte" : "aes67"
+    profileTemplate      = var.template == "telecom" ? "telecom_full_path" : var.template == "smpte" ? "smpte" : var.template == "default" ? "default" : "aes67"
     ptpoeDstMacRxNoMatch = var.mismatch_handling == "configured" ? "replyWithCfgMac" : var.mismatch_handling == "received" ? "replyWithRxMac" : "drop"
     ptpoeDstMacType      = var.forwardable ? "forwardable" : "non-forwardable"
     syncIntvl            = var.sync_interval
