29cb629703d94b73b7a715a17e1de56967609aae
Jose Miguel Hernandez <josemher@cisco.com>
2026-09-14 22:22:01 -0600

    Add support for mgmt ip addr pools

---
+++ modules/terraform-aci-ip-address-pool/main.tf
@@ -0,0 +1,22 @@
+resource "aci_rest_managed" "fvnsAddrInst" {
+  dn         = "uni/tn-${var.tenant}/addrinst-${var.name}"
+  class_name = "fvnsAddrInst"
+  content = {
+    name      = var.name
+    descr     = var.description
+    addr      = var.gateway_address
+    addrType  = "regular"
+    skipGwVal = var.skip_gateway_validation == true ? "yes" : "no"
+  }
+}
+
+resource "aci_rest_managed" "fvnsUcastAddrBlk" {
+  for_each = { for range in var.address_ranges : "${range.from}-${range.to}" => range }
+
+  dn         = "${aci_rest_managed.fvnsAddrInst.dn}/fromaddr-[${each.value.from}]-toaddr-[${each.value.to}]"
+  class_name = "fvnsUcastAddrBlk"
+  content = {
+    from = each.value.from
+    to   = each.value.to
+  }
+}
